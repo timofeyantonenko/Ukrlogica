8cc3b0102fba182f117ef49b9b02de0b2cf28520
Antonenko Timofey <antonenkodev@gmail.com>
2015-08-12 12:35:46 +0300

    Video splash

--- app/src/main/java/com/ukrlogic/android/ukrlogica/Splash.java
+++ app/src/main/java/com/ukrlogic/android/ukrlogica/Splash.java
@@ -2,8 +2,10 @@ package com.ukrlogic.android.ukrlogica;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
+import android.widget.VideoView;
 
 /**
  * Created by root on 11.08.15.
@@ -17,16 +19,45 @@ public class Splash extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-setContentView(R.layout.splash);
+
+        try{
+            VideoView videoHolder = new VideoView(this);
+            setContentView(videoHolder);
+            Uri video = Uri.parse("android.resource://" + getPackageName() + "/"
+                    + R.raw.splash_video);
+            videoHolder.setVideoURI(video);
+
+            videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                public void onCompletion(MediaPlayer mp) {
+                    jump();
+                }
+
+            });
+            videoHolder.start();
+        } catch(Exception ex) {
+            jump();
+        }
+
+        /*setContentView(R.layout.splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
+                // Create an Intent that will start the Menu-Activity.
                 Intent mainIntent = new Intent(Splash.this, MainActivity.class);
                 Splash.this.startActivity(mainIntent);
                 Splash.this.finish();
             }
-        }, SPLASH_DISPLAY_LENGTH);
+        }, SPLASH_DISPLAY_LENGTH);*/
+    }
+
+    private void jump() {
+//it is safe to use this code even if you
+//do not intend to allow users to skip the splash
+        if(isFinishing())
+            return;
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 
 
